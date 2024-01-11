d9acc6e53eecd7f85ab5b96fb8d86b4b8f273df4
Evie Butland <evie.butland@wearehectare.com>
2024-01-11 10:44:05 +1000

    db ideas

--- server/socket.ts
+++ server/socket.ts
@@ -13,7 +13,10 @@ export const socket = io('http://localhost:3050/', { transports: ['websocket'] }
 // id: 2
 // messageThreads: [48]
 
-// Message thread 
+// Message thread / 'conversation'
 // id: 48 (each user will share with)
 // users: [1, 2],
 // thread: [{user: 1, message: 'hello', timestamp: 0:00:00}, {user: 1, message: 'are you there?'}, {user: 2, message: 'yes'}]}
+// https://developer.redis.com/howtos/chatapp/#step-5-running-the-backend
+// https://www.tome01.com/efficient-schema-design-for-a-chat-app-using-postgresql
+// POSTGRES  + REDIS? 
